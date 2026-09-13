9aa585a362718e2dd0141b7d93ad66366317243a
villeilmariaurila-create <villeilmariaurila@gmail.com>
2026-09-13 19:54:19 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,9 @@
 import streamlit as st
 import streamlit.components.v1 as components
 
-st.set_page_config(
-    page_title="Aatoksen ja Villen Forssa-Peli", page_icon="🚜", layout="wide"
-)
-
-st.title("🚜 Aatoksen ja Villen Forssa-Peli: Raviradan Kunnossapito 🐎")
-st.caption("Ohjaa vesiautoa nuolinäppäimillä tai WASD:lla. Kastele rata ja väistä traktoreita!")
+st.set_page_config(page_title="Forssa-Peli", layout="wide")
 
 with open("game.html", "r", encoding="utf-8") as f:
-    game_html = f.read()
+    html_data = f.read()
 
-components.html(game_html, height=600)
+components.html(html_data, height=600)
