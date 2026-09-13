51235b1bfbc391481908bd5670b8005f9ed5da48
villeilmariaurila-create <villeilmariaurila@gmail.com>
2026-09-13 19:26:43 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,10 +5,7 @@ st.set_page_config(
     page_title="Aatoksen ja Villen Forssa-Peli", page_icon="🚜", layout="wide"
 )
 
-st.title("🚜 Aatoksen ja Villen Forssa-Peli: Raviradan Kunnossapito 🚛")
-st.caption(
-    "Ohjaa vesiautoa nuolinäppäimillä tai WASD:lla. Kastele rata ja väistä"
-    " traktoreita!"
-)
+st.title("🚜 Aatoksen ja Villen Forssa-Peli: Raviradan Kunnossapito 🐎")
+st.caption("Ohjaa vesiautoa nuolinäppäimillä tai WASD:lla. Kastele rata ja väistä traktoreita!")
 
 game_html = """
