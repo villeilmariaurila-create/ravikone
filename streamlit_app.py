81c42d513202ba293d5aa6478d36d293256e66c9
villeilmariaurila-create <villeilmariaurila@gmail.com>
2026-09-13 20:07:23 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,3 @@
 import streamlit as st
 import streamlit.components.v1 as components
-
-st.set_page_config(page_title="Forssa-Peli", layout="wide")
-
-with open("game.html", "r", encoding="utf-8") as f:
-    html_data = f.read()
-
-components.html(html_data, height=600)
+components.html(open("game.html", encoding="utf-8").read(), height=600)
